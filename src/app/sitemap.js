20860562d2fbf5159c171b489681dfe4a7826554
Lakshan .R <Lakshan.0714@gmail.com>
2025-11-26 12:45:10 +0530

    meta data added

---
+++ src/app/sitemap.js
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: 'https://xrawinnovations.com', priority: 1 },
+    { url: 'https://xrawinnovations.com/services', priority: 0.9 },
+    { url: 'https://xrawinnovations.com/portfolio', priority: 0.9 },
+    { url: 'https://xrawinnovations.com/contact', priority: 0.8 },
+  ];
+}
